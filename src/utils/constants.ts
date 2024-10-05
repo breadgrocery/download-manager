ca20a5cb4896961a65848f69488268244914822c
Bread Grocery <breadgrocery@gmail.com>
2024-10-06 03:44:10 +0800

    docs: update README

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const constants = {
   about: {
     github: "https://github.com/breadgrocery/download-manager",
-    chrome: `https://chromewebstore.google.com/detail/hhmgnllgjongoenjfnbcdlangemdiodk`,
+    chrome: "https://chromewebstore.google.com/detail/hhmgnllgjongoenjfnbcdlangemdiodk",
     edge: "https://microsoftedge.microsoft.com/addons/detail/jgecopfgpbndacamabkgjhgbepmallpd"
   }
 };
