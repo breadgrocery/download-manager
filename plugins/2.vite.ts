0f8765538e50c7bee0ff69b23c9d14eeaed05626
Bread Grocery <breadgrocery@gmail.com>
2024-10-23 10:23:29 +0800

    chore: integrate `unplugin-vue-components` with `wxt`

---
+++ plugins/2.vite.ts
@@ -0,0 +1,43 @@
+import autoprefixer from "autoprefixer";
+import { resolve } from "node:path";
+import IconsResolver from "unplugin-icons/resolver";
+import Icons from "unplugin-icons/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { addViteConfig, defineWxtModule } from "wxt/modules";
+
+export default defineWxtModule(wxt => {
+  const {
+    reloadConfig,
+    config: { typesDir, mode }
+  } = wxt;
+  const configure = () => {
+    wxt.reloadConfig = () => reloadConfig().then(configure);
+    addViteConfig(wxt, () => ({
+      plugins: [
+        Components({
+          resolvers: [NaiveUiResolver(), IconsResolver({ prefix: "icon" })],
+          dirs: ["components"],
+          dts: resolve(typesDir, "components.d.ts")
+        }),
+        Icons()
+      ],
+      css: {
+        preprocessorOptions: { scss: { api: "modern-compiler" } },
+        postcss: { plugins: [autoprefixer()] }
+      },
+      build: {
+        sourcemap: mode === "development",
+        minify: mode === "production"
+      },
+      esbuild: {
+        drop: mode === "production" ? ["console", "debugger"] : undefined
+      }
+    }));
+  };
+  configure();
+
+  wxt.hooks.hook("prepare:types", async (_, entries) => {
+    entries.push({ module: "./types/components.d.ts" });
+  });
+});
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,22 +1,13 @@
-import autoprefixer from "autoprefixer";
 import { resolve } from "node:path";
 import process from "node:process";
-import IconsResolver from "unplugin-icons/resolver";
-import Icons from "unplugin-icons/vite";
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
-import Components from "unplugin-vue-components/vite";
 import { defineConfig } from "wxt";
-
-const env = {
-  development: process.env.NODE_ENV === "development",
-  production: process.env.NODE_ENV === "production"
-};
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   root: resolve("src"),
   publicDir: resolve("public"),
   entrypointsDir: resolve("src/pages"),
+  modulesDir: resolve("plugins"),
   outDir: resolve("dist"),
   runner: {
     openDevtools: true,
@@ -29,25 +20,5 @@ export default defineConfig({
   zip: {
     artifactTemplate: `${process.env.npm_package_name}-{{browser}}.zip`,
     sourcesTemplate: `${process.env.npm_package_name}-sources.zip`
-  },
-  vite: () => ({
-    plugins: [
-      Components({
-        resolvers: [NaiveUiResolver(), IconsResolver({ prefix: "icon" })],
-        dirs: ["components"]
-      }),
-      Icons()
-    ],
-    css: {
-      preprocessorOptions: { scss: { api: "modern-compiler" } },
-      postcss: { plugins: [autoprefixer()] }
-    },
-    build: {
-      sourcemap: env.development,
-      minify: env.production
-    },
-    esbuild: {
-      drop: env.production ? ["console", "debugger"] : undefined
-    }
-  })
+  }
 });
