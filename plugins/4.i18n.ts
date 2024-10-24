76e3b94110a66b4dcea4919663e12c7a011b1683
Bread Grocery <breadgrocery@gmail.com>
2024-10-24 10:34:16 +0800

    chore: update locales dir

--- plugins/4.i18n.ts
+++ plugins/4.i18n.ts
@@ -1,3 +1,13 @@
 import module from "@wxt-dev/i18n/module";
+import { resolve } from "node:path";
+import { defineWxtModule } from "wxt/modules";
 
-export default module;
+export default defineWxtModule({
+  ...module,
+  setup: (wxt, options) => {
+    module.setup?.(wxt, {
+      localesDir: resolve("locales"),
+      ...options
+    });
+  }
+});
